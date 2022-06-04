b46c40ff300cba9e443454b317f62133f2fea957
AlgoRythm-Dylan <dperunko@comcast.net>
2022-06-03 20:57:34 -0400

    Started adding unit and performance test support

--- src/tests/lani.test.js
+++ src/tests/lani.test.js
@@ -1,7 +1,35 @@
 Lani.installedModules.push("lani-testing");
 
+Lani.UnitTestError = class extends Error {
+    constructor(message){
+        super(message);
+    }
+}
+
+Lani.assertEqual = (a, b) => {
+    if(a !== b)
+        throw new Lani.UnitTestError(`${a} is not equal to ${b}`);
+}
+
 Lani.UnitTest = class {
     constructor(){
         
     }
+    evaluate(){
+
+    }
 };
+
+Lani.PerformanceTest = class {
+    constructor(){
+        this.iterations = 1000000;
+        this.start = null;
+        this.end = null;
+    }
+    test(){
+
+    }
+    get time(){
+        return this.end - this.start;
+    }
+}
