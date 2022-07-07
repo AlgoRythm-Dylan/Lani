4eae26a2d5e39fc5556011d0527f1cc22964b1cb
AlgoRythm-Dylan <dperunko@comcast.net>
2022-07-07 08:33:23 -0400

    Implement levenshtein distance

--- src/search.js
+++ src/search.js
@@ -14,5 +14,20 @@ Lani.search.includes = (term, dataSet, caseInsensitive=true) => {
 Lani.search.splitWords = str => str.match(/\b(\w+)'?(\w+)?\b/);
 
 Lani.search.levenshtein = (str1, str2) => {
+    if(str1.length === 0)
+        return str2.length;
+    else if(str2.length === 0)
+        return str1.length;
+    if(str1[0] === str2[0])
+        return Lani.search.levenshtein(Lani.search.levenshteinTail(str1), Lani.search.levenshteinTail(str2));
+    else
+        return 1 + Math.min(
+            Lani.search.levenshtein(Lani.search.levenshteinTail(str1), str2),
+            Lani.search.levenshtein(str1, Lani.search.levenshteinTail(str2)),
+            Lani.search.levenshtein(Lani.search.levenshteinTail(str1), Lani.search.levenshteinTail(str2))
+        );
+}
 
+Lani.search.levenshteinTail = str => {
+    return str.slice(1);
 }
