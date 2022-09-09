3746b735cda1504d380ac36b85d8485c280c9e1c
AlgoRythm-Dylan <dperunko@comcast.net>
2022-09-08 23:58:46 -0400

    markdown?

---
+++ src/markdown.js
@@ -0,0 +1,27 @@
+Lani.installedModules.push("lani-markdown");
+
+Lani.MarkdownTokenType = {
+
+}
+
+Lani.MarkdownToken = class {
+    constructor(type=null){
+        this.type = type;
+    }
+}
+
+Lani.MarkdownTokenizer = class {
+
+}
+
+Lani.MarkdownParser = class {
+    nextSymbol(){
+        
+    }
+    accept(){
+
+    }
+    expect(){
+
+    }
+}
